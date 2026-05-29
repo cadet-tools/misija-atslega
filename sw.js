9b68e1856d1dbf6f872281c438387039ffcf712d
cadet-tools <eglis.rjsv@gmail.com>
2026-05-29 23:08:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// GitHub Pages versija: service worker atslēgts, lai nerādītu vecu kešotu HTML.
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
